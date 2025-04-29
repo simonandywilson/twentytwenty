65ea99d8f5fb4437be7a638db90db3ae62306cc8
simonandywilson <simon@soup.work>
2025-04-29 14:10:12 +0100

    Enhance CSS styles and update artist text content:
    - Added a new `.inline` class for inline display in styles.css.
    - Refined padding classes for consistency in styles.
    - Removed outdated aspect ratio styles for iframes to streamline CSS.
    - Updated artist text content to include an accessible video format and improved narrative structure in artist.txt.
    - Enhanced partner reflections to better articulate collaborative processes and research outcomes.

--- site/snippets/blocks/accessible-video.php
+++ site/snippets/blocks/accessible-video.php
@@ -5,7 +5,7 @@
       class="absolute top-0 left-0 w-full h-full border-0"
       marginwidth="0"
       marginheight="0"
-      src="//plugin.3playmedia.com/show?ad=1&ad_autoplay=0&ad_default_source_volume_control=0&ad_source_volume_control=0&cc=1&cc_minimizable=0&cc_minimize_on_load=1&cc_multi_text_track=0&cc_overlay=0&cc_searchable=0&embed=iframe&height=auto&itx=1&itx_collapse_on_load=1&itx_collapsible=1&itx_downloadable=0&itx_highlight_by_caption_frames=0&itx_keywords=0&itx_light_scroll=0&itx_multi_text_track=0&itx_progress_bar=0&itx_progressive_tracking=0&mf=12961354&p3sdk_version=1.11.7&p=67226&player_type=youtube&plugin_skin=light&video_id=<?= $block->videoId()->esc() ?>&video_target=tpm-plugin-k9mg9lks-<?= $block->videoId()->esc() ?>&width=auto"
+      src="//plugin.3playmedia.com/show?ad=1&ad_autoplay=0&ad_default_source_volume_control=0&ad_source_volume_control=0&cc=1&cc_minimizable=0&cc_minimize_on_load=1&cc_multi_text_track=0&cc_overlay=0&cc_searchable=0&embed=iframe&height=auto&itx=1&itx_collapse_on_load=1&itx_collapsible=1&itx_downloadable=0&itx_highlight_by_caption_frames=0&itx_keywords=0&itx_light_scroll=0&itx_multi_text_track=0&itx_progress_bar=0&itx_progressive_tracking=0&p3sdk_version=1.11.7&p=67226&player_type=youtube&plugin_skin=light&video_id=<?= $block->videoId()->esc() ?>&width=auto"
       title="Video Player"
       <?php if ($block->caption()->isNotEmpty()) : ?>
       aria-describedby="vid-caption-<?= $block->id() ?>"
